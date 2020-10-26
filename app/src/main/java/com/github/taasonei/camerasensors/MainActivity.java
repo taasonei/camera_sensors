c78ec7d858f2af12d0d16851670194130401d3b5
taasonei <taasonei@gmail.com>
2020-10-26 22:22:04 +0300

    replaced GraphView with MPAndroidChart

--- app/src/main/java/com/github/taasonei/camerasensors/MainActivity.java
+++ app/src/main/java/com/github/taasonei/camerasensors/MainActivity.java
@@ -4,47 +4,67 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.ContactsContract;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.Viewport;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private final Activity activity = this;
     private static final Random RANDOM = new Random();
-    private LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-    private int lastX = 0;
-    private Viewport viewport;
-    private Handler mHandler = new Handler();
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final GraphView graph = (GraphView) findViewById(R.id.graph);
+        chart = findViewById(R.id.chart);
 
-        graph.addSeries(series);
+        chart.setDescription("");
+        chart.setNoDataTextDescription("No data for the moment");
+        chart.setHighlightEnabled(true);
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setPinchZoom(true);
+        chart.setBackgroundColor(Color.LTGRAY);
 
-        viewport = graph.getViewport();
-        viewport.setYAxisBoundsManual(true);
-        viewport.setMinY(0);
-        viewport.setMaxY(10);
-        // activate horizontal scrolling  enables horizontal scrolling
-       viewport.setScrollable(true);
-        // activate horizontal zooming and scrolling
-       //viewport.setScalable(true);
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+        chart.setData(data);
+
+        Legend l = chart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = chart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawAxisLine(false);
+        xl.setAvoidFirstLastClipping(true);
+
+        YAxis yl = chart.getAxisLeft();
+        yl.setTextColor(Color.WHITE);
+        yl.setAxisMaxValue(120f);
+        yl.setDrawGridLines(true);
+
+        YAxis yl2 = chart.getAxisRight();
+        yl2.setEnabled(false);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
         bottomNavigation.setOnNavigationItemSelectedListener(
@@ -53,65 +73,77 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.iso:
-                                graph.setVisibility(View.VISIBLE);
 
                                 break;
                             case R.id.focus:
-                                graph.setVisibility(View.INVISIBLE);
                                 break;
                             case R.id.accelerometer:
-                                graph.setVisibility(View.INVISIBLE);
                                 break;
                             case R.id.coordinates:
-                                graph.setVisibility(View.INVISIBLE);
                                 break;
                             case R.id.approximation:
-                                graph.setVisibility(View.INVISIBLE);
                                 break;
                         }
                         return true;
                     }
                 }
         );
-        addRandomData();
     }
-
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 15; i++) {
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            addEntry();
-//                        }
-//                    });
-//                    try {
-//                        Thread.sleep(600);
-//                    } catch (InterruptedException e) {
-//
-//                    }
-//                }
-//            }
-//        }).start();
-//    }
 
     private void addEntry() {
-        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), false, 22);
-       //viewport.scrollToEnd();
+        LineData data = chart.getData();
+
+        if (data != null) {
+            LineDataSet set = data.getDataSetByIndex(0);
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+            data.addXValue(new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date()));
+            data.addEntry(new Entry((float) (Math.random() * 75) + 60f, set.getEntryCount()), 0);
+            chart.notifyDataSetChanged();
+            chart.setVisibleXRange(6);
+            chart.moveViewToX(data.getXValCount() - 7);
+        }
     }
 
-    private void addRandomData() {
-        mHandler.postDelayed(new Runnable() {
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "SPL Db");
+        set.setDrawCubic(true);
+        set.setCubicIntensity(0.2f);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setCircleColor(ColorTemplate.getHoloBlue());
+        set.setLineWidth(2f);
+        set.setCircleSize(4f);
+        set.setFillAlpha(65);
+        set.setHighLightColor(Color.rgb(244,117,177));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(10f);
+        return set;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                series.appendData(new DataPoint(lastX++, RANDOM.nextInt(10)), false, 100);
-                addRandomData();
+                for (int i = 0; i < 100; i++) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
             }
-        }, 1000);
+        }).start();
     }
 }
